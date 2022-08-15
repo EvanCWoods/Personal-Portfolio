0e9f57a6fea26a385ed0ba854bccfc1bc3dc2f20
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-08-15 19:43:57 +1000

    added projects to router in app

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./Styles/root.css";
 import "./Styles/colors.css";
 import { LandingPage } from './Components/LandingPage/LandingPage';
+import ProjectsPage from "./Components/ProjectsPage/ProjectsPage";
 import Footer from "./Components/Footer";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
         </Routes>
         <Footer />
       </Router>
