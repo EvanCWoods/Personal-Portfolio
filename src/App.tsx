1d6affa7f0d0e3350db3c2cfada501af16d3f385
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-08-16 19:01:30 +1000

    removed progress, restarting with different design

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./Styles/root.css";
-import "./Styles/colors.css";
-import { LandingPage } from './Components/LandingPage/LandingPage';
-import ProjectsPage from "./Components/ProjectsPage/ProjectsPage";
-import AboutMe from "./Components/AboutMe/AboutMe";
-import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/" element={<AboutMe />} />
-          <Route path="/projects" element={<ProjectsPage />} />
         </Routes>
-        <Footer />
       </Router>
     </div>
   );
