24b8610f2dd5bdaaf2e0b3b162a10bc0a4a60a5f
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-08-21 20:38:41 +1000

    added location content to side bar

--- src/Components/Landing/Components/SideBar.tsx
+++ src/Components/Landing/Components/SideBar.tsx
@@ -14,6 +14,21 @@ export default function SideBar() {
           <p className="sidebar-text">(MERN &amp; ASP .NET)</p>
         </div>
       </div>
+      <div className="location">
+        <div className="country">
+          <p className="location-key">Country:</p>
+          <p className="location-value">Australia</p>
+        </div>
+        <div className="city">
+          <p className="location-key">City:</p>
+          <p className="location-value">Sydney</p>
+        </div>
+        <div className="age">
+          <p className="location-key">Age:</p>
+          <p className="location-value">21</p>
+        </div>
+        <div className="underline"></div>
+      </div>
     </div>
   );
 }
