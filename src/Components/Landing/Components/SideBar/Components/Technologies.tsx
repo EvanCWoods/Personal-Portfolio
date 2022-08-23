436bcf5a4397c6f60d2aed861d114baeb426cfad
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-08-23 18:20:16 +1000

    added mui, added a technologies component, added MUI toggle button to show things im skilled in or things I'm learning

--- src/Components/Landing/Components/SideBar/Components/Technologies.tsx
+++ src/Components/Landing/Components/SideBar/Components/Technologies.tsx
@@ -1,7 +1,28 @@
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+import { useState } from "react";
+
 export default function Technologies() {
-    return (
-        <div className="technologies">
-            
-        </div>
-    )
+    const [alignment, setAlignment] = useState("skilled");
+
+  const handleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string,
+  ) => {
+    setAlignment(newAlignment);
+  };
+
+  return (
+    <div className="technologies">
+      <ToggleButtonGroup
+        color="primary"
+        value={alignment}
+        exclusive
+        onChange={handleChange}
+        aria-label="Platform"
+      >
+        <ToggleButton value="skilled">Skilled</ToggleButton>
+        <ToggleButton value="learning">Leaning</ToggleButton>
+      </ToggleButtonGroup>
+    </div>
+  );
 }
