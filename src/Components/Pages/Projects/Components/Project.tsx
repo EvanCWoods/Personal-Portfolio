ede2b9e23a384d76c282ac35c46963e030e3feea
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-08-27 08:35:04 +1000

    added basic content to component to style

--- src/Components/Pages/Projects/Components/Project.tsx
+++ src/Components/Pages/Projects/Components/Project.tsx
@@ -3,7 +3,13 @@ import "../../../../Styles/Projects/Project.css";
 export default function Project() {
     return (
     <div className="project">
-
+        <div className="image-container">
+        <img className="project-image" src={""} alt="some project" />
+        </div>
+        <h1>title</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa soluta ipsam ab sequi necessitatibus cum reprehenderit optio, deleniti neque temporibus veniam illo commodi similique, repudiandae voluptatum pariatur modi voluptates assumenda!</p>
+        <a href="/">See The Site!</a>
+        <a href="/">See The Code!</a>
     </div>
     )
 }
