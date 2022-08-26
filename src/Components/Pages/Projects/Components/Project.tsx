24bd2044a22e6d89ec2889d0cf12761a645b10cf
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-08-27 08:49:43 +1000

    got styling for the project component to be working

--- src/Components/Pages/Projects/Components/Project.tsx
+++ src/Components/Pages/Projects/Components/Project.tsx
@@ -1,15 +1,22 @@
 import "../../../../Styles/Projects/Project.css";
 
 export default function Project() {
-    return (
+  return (
     <div className="project">
-        <div className="image-container">
+      <div className="image-container">
         <img className="project-image" src={""} alt="some project" />
-        </div>
-        <h1>title</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa soluta ipsam ab sequi necessitatibus cum reprehenderit optio, deleniti neque temporibus veniam illo commodi similique, repudiandae voluptatum pariatur modi voluptates assumenda!</p>
+      </div>
+      <h1>title</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa soluta
+        ipsam ab sequi necessitatibus cum reprehenderit optio, deleniti neque
+        temporibus veniam illo commodi similique, repudiandae voluptatum
+        pariatur modi voluptates assumenda!
+      </p>
+      <div className="external-link-container">
         <a href="/">See The Site!</a>
         <a href="/">See The Code!</a>
+      </div>
     </div>
-    )
+  );
 }
--- src/Components/Pages/Projects/WebDevelopment/WebDevelopmentProjects.tsx
+++ src/Components/Pages/Projects/WebDevelopment/WebDevelopmentProjects.tsx
@@ -1,9 +1,9 @@
 import Project from "../Components/Project";
 import "../../../../Styles/Projects/Projects.css";
 export default function WebDevelopmentProjects() {
-    return(
-        <div className="web-development projects">
-            <Project />
-        </div>
-    )
+  return (
+    <div className="web-development projects">
+      <Project />
+    </div>
+  );
 }
