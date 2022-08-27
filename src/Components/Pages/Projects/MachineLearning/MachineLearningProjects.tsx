21685587ada40b20110e43ea41b21402fb2fddc3
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-08-27 21:00:12 +1000

    fixed content in machine learning projects page and updated web 3 projects

--- src/Components/Pages/Projects/MachineLearning/MachineLearningProjects.tsx
+++ src/Components/Pages/Projects/MachineLearning/MachineLearningProjects.tsx
@@ -1,5 +1,5 @@
 import Project from "../Components/Project";
-import Mushroom from "../../../../Assets/ProjectImages/Mushroom.jpeg"
+import Mushroom from "../../../../Assets/ProjectImages/Mushroom.jpeg";
 
 export default function MachineLearningProjects() {
   return (
@@ -10,7 +10,12 @@ export default function MachineLearningProjects() {
         </h1>
       </div>
       <div className="project-cards">
-          <Project image={Mushroom} title="Mushroom Classifier" description="This project was built using TensorFlow in Python. The aim was to be able to take in alphaNumeric values as input and train a nerual network on those inputs to accurately classify a mushroom as being either poisonous or safe to eat." codeLink="https://github.com/EvanCWoods/Mushroom-Classifier"/>
+        <Project
+          image={Mushroom}
+          title="Mushroom Classifier"
+          description="This project was built using TensorFlow in Python. The aim was to be able to take in alphaNumeric values as input and train a nerual network on those inputs to accurately classify a mushroom as being either poisonous or safe to eat."
+          codeLink="https://github.com/EvanCWoods/Mushroom-Classifier"
+        />
       </div>
     </div>
   );
--- src/Components/Pages/Projects/Web3/webThreeProjects.tsx
+++ src/Components/Pages/Projects/Web3/webThreeProjects.tsx
@@ -1,5 +1,12 @@
 export default function WebThreeProjects() {
-    return(
-        <div className="Web-three projects"></div>
-    )
+  return (
+    <div className="Web-three projects">
+      <div className="section-header">
+        <h1>
+          My <span className="underline-title">Web 3.0 Work</span>
+        </h1>
+      </div>
+      <div className="project-cards"></div>
+    </div>
+  );
 }
