4c6388657e6f60fae89391057d4dc4153f2db28d
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-11-05 14:21:08 +1100

    added contact modal

--- src/Components/Landing/Components/CallToAction/CallToAction.tsx
+++ src/Components/Landing/Components/CallToAction/CallToAction.tsx
@@ -2,8 +2,16 @@ import { Box, Typography } from "@mui/material";
 import Mountains from "../../../../Assets/mountain-landscape.png";
 import "../../../../Styles/Landing/CallToAction.css";
 import Work from "../Work/Work";
+import ContactModal from "../Modals/ContactModal";
+import { useState } from "react";
 
 export default function CallToAction() {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleOpenModal = () => {
+    setOpen(true);
+  }
+
   return (
     <Box sx={{ width: "100%", height: "60%" }}>
       <Box
@@ -17,22 +25,22 @@ export default function CallToAction() {
         <Box
           className="blue-background"
           sx={{
-            width: "70%",
+            width: "80%",
             height: "90%",
             position: "relative",
             top: "10%",
-            mx: "15%"
+            mx: "10%"
           }}>
 
           <Box
             sx={{
-              width: "130%",
-              height: "100%",
+              width: "120%",
+              height: "110%",
               backgroundImage: `url(${Mountains})`,
               backgroundSize: "cover",
               position: "relative",
-              top: "10%",
-              left: "-15%"
+              top: "7%",
+              left: "-10%"
             }}>
             <Box
               className="blue-background"
@@ -41,6 +49,8 @@ export default function CallToAction() {
                 height: "100%",
                 opacity: "0.7"
               }}>
+              <Typography variant="h4" sx={{ color: "var(--white-text)" }}>Inquire To See What I Can Do For You</Typography>
+              <button style={{ padding: "1% 2%", border: "none", background: "var(--yellow-background)", borderRadius: "10px" }} onClick={handleOpenModal}>ENQUIRE</button>
             </Box>
           </Box>
         </Box>
@@ -48,6 +58,7 @@ export default function CallToAction() {
       <Box className="blue-background" sx={{ paddingTop: "10%" }}>
         <Work />
       </Box>
+      <ContactModal open={open} setOpen={setOpen} />
     </Box>
   );
 }
