d6f78ce78bd10a29317719b981c9d3f5e0a4a0e3
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2023-01-02 20:58:31 +1100

    fixed styling and added new section for blog content

--- src/Components/Landing/Components/Work/Work.tsx
+++ src/Components/Landing/Components/Work/Work.tsx
@@ -1,14 +1,20 @@
 import WorkPortal from "./Components/WorkPortal";
 import "../../../../Styles/Landing/Work.css";
+import { Box } from "@mui/material";
 
 export default function Work() {
   return (
-    <div>
+    <Box sx={{ display: "flex", justifyContent: "space-evenly" }}>
       <WorkPortal
         title="Web Development"
         content="I am a full stack dev (MERN &amp; ASP.NET) who is driven to bring businesses the power of modern technologies!"
         toUrl="/projects/web-development"
       />
-    </div>
+      <WorkPortal
+        title="Tech Blog"
+        content="See my weekly articles on modern technologies as I discover interesting things about them."
+        toUrl="/blog"
+      />
+    </Box>
   );
 }
