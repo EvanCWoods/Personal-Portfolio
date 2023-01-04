d53ee346fb682bad8dc7a4fe4dd2c7353e85a36b
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2023-01-04 14:25:34 +1100

    fixed styling for the articles

--- Client/src/Components/Blog/Post.tsx
+++ Client/src/Components/Blog/Post.tsx
@@ -25,11 +25,14 @@ const Post = () => {
         <Box sx={{ height: "100vh" }}>
             {post
                 ? <Box sx={{ width: "80%", mx: "10%", display: "flex", justifyContent: "center", flexWrap: "wrap" }}>
-                    <Box sx={{ background: "var(--grey-background)" }}>
-                        <img src={post.thumbnail} alt={post.thumbnailAlt} style={{ width: "90%", margin: "0 5%", maxWidth: "900px" }} />
-                        <Typography variant="h4">{post!.title}</Typography>
-                        <Typography variant="subtitle1">{post!.subtitle}</Typography>
-                        <Typography variant="body1">{post!.body}</Typography>
+                    <Box sx={{ background: "var(--grey-background)", color: "var(--white-text)", pb: "70px" }}>
+                        <img src={post.thumbnail} alt={post.thumbnailAlt} style={{ width: "90%", margin: "5%", maxWidth: "900px", maxHeight: "500px" }} />
+                        <Box sx={{ margin: "0 5%" }}>
+                            <Typography variant="h4">{post!.title}</Typography>
+                            <Typography variant="subtitle1">{post!.subtitle}</Typography>
+                            <br></br>
+                            <Typography variant="body1">{post!.body}</Typography>
+                        </Box>
                     </Box>
                 </Box>
                 : null}
